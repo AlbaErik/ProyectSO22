2b7116fb153e34e60e1bdfd78f5314ef04877a92
AlbaErik <erik.alba97@gmail.com>
2022-11-16 15:43:05 -0300

    cambios minishell

--- MostrarAyuda.c
+++ MostrarAyuda.c
@@ -1,21 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "MostrarAyuda.h"
 
 int main(int argc, char* argv[]){
 
-	printf("\nCOMANDOS:\n");
-	printf("1 - CrearDirectorio 'nombre': Crea un directorio con el nombre del parametro\n");
-	printf("2 - EliminarDirectorio 'nombre': Elimina el directorio correspondiente al nombre del parametro\n");
-	printf("3 - CrearArchivo 'nombre': Crea un archivo de texto con el nombre del parametro\n");
-	printf("4 - ListarDirectorio 'nombre': Lista el contenido del directorio correspondiente al nombre del parametro\n");
-	printf("5 - MostrarArchivo 'nombre.txt': Muestra el contenido del archivo correspondiente al nombre del parametro\n");
-	printf("6 - ModificarPermisosArchivo 'nombre': Modifica los permisos del archivo correspondiente al nombre del parametro\n");
-	printf("7 - MostrarAyuda: Muestra el texto con la ayuda\n");
-	printf("8 - SalirShell: Finaliza la ejecucion\n\n");
-	printf("IMPORTANTE: Es debe especificar la ruta completa para trabajar con archivos\n");
-	printf("EJEMPLO: Para eliminar un directorio se debe realizar la siguiente llamada: EliminarDirectorio /home/user/Desktop/'nombre del directorio a eliminar'\n");
-	printf("Los argumentos deberan estar separados por un espacio\n");
+	printf("\nLista de comandos:\n");
+	printf(" <CrearDirectorio> 'nombre': Crea un directorio con el nombre del parametro\n");
+	printf(" <CrearArchivo> 'nombre': Crea un archivo de texto con el nombre del parametro\n");
+	printf(" <MostrarArchivo> 'nombre': Muestra el contenido de 'nombre'\n");
+	printf(" <ListarDirectorio> 'nombre': Lista el contenido del directorio correspondiente al nombre del parametro\n");
+	printf(" <EliminarDirectorio> 'nombre': Elimina el directorio correspondiente al nombre del parametro\n");
+	printf(" <ModificarPermisosArchivo> 'nombre' 'permisos': Modifica los permisos de 'nombre' con los nuevos permisos 'permisos'\n");
+	printf(" <MostrarAyuda> : Muestra el texto con la ayuda\n\n");
+	printf("Se debe especificar la ruta completa para trabajar con archivos\n");
 	printf("\n");
 
 	return 0;
